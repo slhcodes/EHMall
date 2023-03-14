55e437f614ea4a0a0f9de3be4a79626ee7928d17
abcd <1205126916@qq.com>
2023-03-14 18:41:03 +0800

    --task=1016597 --user=施立豪  获取手机号接口添加RSA加密

---
+++ src/main/java/com/example/ehmall/Util/RsaUtil/RsaServerUtilsImpl.java
@@ -0,0 +1,25 @@
+package com.example.ehmall.Util.RsaUtil;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RsaServerUtilsImpl implements RsaServerUtils {
+    @Override
+    public byte[] encrypt(String plainText, String publicKey) {
+        byte[] cipherText = null;
+        try {
+            byte[] plainTextBytes = plainText.getBytes(StandardCharsets.UTF_8);
+            PublicKey rsaPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec( Base64.getDecoder().decode(publicKey)));
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, rsaPublicKey);
+            cipherText = cipher.doFinal(plainTextBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cipherText;
+    }
+}
--- src/main/java/com/example/ehmall/controller/GetPhoneNumberController.java
+++ src/main/java/com/example/ehmall/controller/GetPhoneNumberController.java
@@ -1,10 +1,14 @@
 package com.example.ehmall.controller;
 
+import com.example.ehmall.Util.RsaUtil.RsaServerUtils;
+import com.example.ehmall.Util.RsaUtil.RsaServerUtilsImpl;
 import com.example.ehmall.service.impl.GetPhoneNumberServiceImpl;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * 2023/3/13 16:41
@@ -16,9 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/phonenumber")
 public class GetPhoneNumberController
 {   @GetMapping("/getnumber")
-    public String GetNumber(String token) throws Exception {
+    public byte[] GetNumber(String token,String publicKey) throws Exception {
         GetPhoneNumberServiceImpl getPhoneNumberService=new GetPhoneNumberServiceImpl();
-        return getPhoneNumberService.GetPhoneNumber(token);
+    // 服务器要发送的信息
+    String info = getPhoneNumberService.GetPhoneNumber(token);
+    // 服务器收到公钥后将信息加密
+    RsaServerUtils rsaServerUtils = new RsaServerUtilsImpl();
+    byte[] cipherText = rsaServerUtils.encrypt(info, publicKey);
+    System.out.println(Arrays.toString(cipherText));
+        return cipherText;
     }
 
 }
